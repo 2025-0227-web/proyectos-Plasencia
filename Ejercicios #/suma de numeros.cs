e54472e139e5bb498acbc4111dda7f5f5029cb91
Junior <2025-0227@unad.edu.do>
2026-06-04 22:01:17 -0400

    Agregar archivos de proyecto.

---
+++ Ejercicios #/suma de numeros.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int suma = 0;
+        int contador = 1;
+
+        // El ciclo se repite mientras el contador no pase de 100
+        while (contador <= 100)
+        {
+            suma = suma + contador; // Sumamos el número actual
+            contador++;             // Pasamos al siguiente número
+        }
+
+        Console.WriteLine("La suma de los primeros 100 números es: " + suma);
+    }
+}
